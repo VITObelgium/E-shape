c65a93b96de58a24a40b9a001adfb3e793735922
tbanyai <tam.banyai@gmail.com>
2021-04-16 15:53:41 +0200

    Adding setup.py (as agreed Kasper as contact)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="E-shape",
+    author="Kasper Bonte",
+    author_email="kasper.bonte@vito.be",
+    description=("E-shape crop calendar"),
+    url='',
+    license="Property of VITO NV"
+)
