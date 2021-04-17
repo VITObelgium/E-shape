b16141df3fc978b6bdcafb6919fb4cd70350473a
tbanyai <tam.banyai@gmail.com>
2021-04-17 08:39:49 +0200

    properly setting up package in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     author_email="kasper.bonte@vito.be",
     description=("E-shape crop calendar"),
     url='',
-    license="Property of VITO NV"
+    license="Property of VITO NV",
+    package_dir={'Crop_calendars': 'Pilot1/src/Crop_calendars'},
+    packages=['Crop_calendars'],
 )
