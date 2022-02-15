61ffb8a5c7eef8afed1a9a54d3f820a78035aecc
Bonte Kasper <kasper.bonte@vito.be>
2022-02-15 13:36:34 +0100

    Remove replace argument but work with context parameter

--- tests/resources/UDF_biopar_calculation_shub_3_band.py
+++ tests/resources/UDF_biopar_calculation_shub_3_band.py
@@ -1,39 +1,21 @@
-# import openeo
-# import scipy.signal
-# import shapely
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""OpenEO Python UDF interface"""
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
-
-# from Products.Indices.s2_20m_biopar_processor import S2BioparProcessor
 import numpy as np
-# from openeo_udf.api.structured_data import StructuredData
 from typing import Dict
-# import xarray
-# from scipy.signal import savgol_filter
-from openeo_udf.api.datacube import DataCube
+from openeo.udf.xarraydatacube import XarrayDataCube
 import tensorflow as tf
-
-# from openeo_udf.api.udf_data import UdfData
-biopar_version = '3band'
-import sys
-
-sys.path.append(r'/data/users/Public/bontek/Nextland/Biopar_test/wheel/biopar-1.2.0-py3-none-any.whl')
 from biopar.bioparnnw import BioParNNW
 
 
-def apply_datacube(cube: DataCube, context: Dict) -> DataCube:
-    biopar = $BIOPAR
+biopar_version = '3band'
+
+def apply_datacube(cube: XarrayDataCube, context: Dict) -> XarrayDataCube:
+    valid_biopars= ['FAPAR','LAI','FCOVER','CWC','CCC']
+    biopar = context.get("biopar", "FAPAR")
+    if biopar not in valid_biopars:
+        biopar = 'FAPAR'
+
     ds = cube.get_array()
     ds_date = ds
-    # dates = list(ds.coords['t'].values)
 
-    # ds_date = ds.sel(t=date)
     from numpy import cos, radians
     ### LOAD THE DIFFERENT REQUIRED BANDS FOR THE 8-BAND FAPAR
     scaling_bands = 0.0001
@@ -47,7 +29,6 @@ def apply_datacube(cube: DataCube, context: Dict) -> DataCube:
     B04 = ds_date.sel(bands='B04') * scaling_bands
     B8 = ds_date.sel(bands='B08') * scaling_bands
 
-
     g1 = cos(radians(vza))
     g2 = cos(radians(sza))
     g3 = cos(radians(saa - vaa))
@@ -59,25 +40,13 @@ def apply_datacube(cube: DataCube, context: Dict) -> DataCube:
 
     #### CALCULATE THE BIOPAR BASED ON THE BANDS #####
     image = BioParNNW(version='3band', parameter=biopar, singleConfig = True).run(bands, output_scale=1,
-                                                                  output_dtype=tf.dtypes.float32,
-                                                                  minmax_flagging=False)  # netcdf algorithm
+                                                                                  output_dtype=tf.dtypes.float32,
+                                                                                  minmax_flagging=False)  # netcdf algorithm
     as_image = image.reshape((g1.shape))
     ## set nodata to nan
     as_image[np.where(np.isnan(B03))] = np.nan
     xr_biopar = vza.copy()
     xr_biopar.values = as_image
 
-    return DataCube(xr_biopar)  # xarray.DataArray(as_image,vza.dims,vza.coords)
-
-###### OLD CODE #######
-# processor = S2BioparProcessor(basepath= r'data', output_dir= r'data', parameter = 'FAPAR')
-# variable_array = processor.compute_biopar_S2(ds_date)[::-1]
-# mean_time = np.nanmean(variable_array)
-# dict_biopar_mean_time.update({date: mean_time})
-# udf_data.set_structured_data_list([StructuredData(description="fAPAR_mean json", data=dict_biopar_mean_time, type="dict")])
-### how to drop the bands of the xarray
-# ds.where(ds.bands == 'RAA_60M', drop=True)
-
-
-
+    return XarrayDataCube(xr_biopar)  # xarray.DataArray(as_image,vza.dims,vza.coords)
 
