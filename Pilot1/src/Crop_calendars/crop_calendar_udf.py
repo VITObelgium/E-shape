8e6e234a9c24015dcc3589fca3a77538474493d2
Bonte Kasper <kasper.bonte@vito.be>
2022-02-16 08:02:26 +0100

    remove string for filtering on columns location

--- Pilot1/src/Crop_calendars/crop_calendar_udf.py
+++ Pilot1/src/Crop_calendars/crop_calendar_udf.py
@@ -18,15 +18,15 @@ from pathlib import Path
 ######## FUNCTIONS ################
 def get_cropsar_TS(ts_df, unique_ids_fields, metrics_order, fAPAR_rescale_Openeo, shub, Spark = True):
     index_fAPAR = metrics_order.index('fAPAR')
-    df_S2 = ts_df.loc[:, ts_df.columns.get_level_values(1).isin([str(index_fAPAR)])].sort_index().T
+    df_S2 = ts_df.loc[:, ts_df.columns.get_level_values(1).isin([index_fAPAR])].sort_index().T
     if not shub:
         df_S2 *= fAPAR_rescale_Openeo
     index_S1_ascending = metrics_order.index('sigma_ascending_VH')
     df_S1_ascending = ts_df.loc[:, ts_df.columns.get_level_values(1).isin(
-        [str(index_S1_ascending), str(index_S1_ascending + 1), str(index_S1_ascending + 2)])].sort_index().T
+        [index_S1_ascending, index_S1_ascending + 1, index_S1_ascending + 2])].sort_index().T
     index_S1_descending = metrics_order.index('sigma_descending_VH')
     df_S1_descending = ts_df.loc[:, ts_df.columns.get_level_values(1).isin(
-        [str(index_S1_descending), str(index_S1_descending + 1), str(index_S1_descending + 2)])].sort_index().T
+        [index_S1_descending, index_S1_descending + 1, index_S1_descending + 2])].sort_index().T
     if Spark:
         if shub:
             cropsar_df, cropsar_df_q10, cropsar_df_q90 = run_cropsar_dataframes(df_S2, df_S1_ascending,
